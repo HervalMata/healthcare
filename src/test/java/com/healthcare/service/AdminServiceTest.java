235cf1e44dba663d9e0722107674959eb9025fb5
Danny <dzhang@operr.com>
2017-05-05 18:31:41 -0400

    DAO files added

--- src/test/java/com/healthcare/service/AdminServiceTest.java
+++ src/test/java/com/healthcare/service/AdminServiceTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.healthcare.model.entity.Admin;
 import com.healthcare.service.AdminService;
 
 @RunWith(SpringRunner.class)
