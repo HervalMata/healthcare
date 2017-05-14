46460aa342d3fd702481a4be5427e33d94a49d8e
Maxim S <atlamax@gmail.com>
2017-05-14 23:28:18 +0300

    CRUD for Meal/Activity/Review:
    Not final version of configuration for integration tests.

--- src/integration-test/java/com/healthcare/IntegrationTestConfiguration.java
+++ src/integration-test/java/com/healthcare/IntegrationTestConfiguration.java
@@ -1,12 +1,17 @@
 package com.healthcare;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @TestPropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.healthcare")
+@ActiveProfiles("integration-test")
 public class IntegrationTestConfiguration {
 
 }
--- src/integration-test/resources/data.sql
+++ src/integration-test/resources/data.sql
@@ -1,2 +1,8 @@
+INSERT INTO health_care_v1_dev.user (id)
+  VALUES (2);
+
+INSERT INTO health_care_v1_dev.user (id)
+  VALUES (4);
+
 INSERT INTO health_care_v1_dev.review (id, user_id, employee_id, user_id1)
   VALUES (1, 2, 3, 4);
