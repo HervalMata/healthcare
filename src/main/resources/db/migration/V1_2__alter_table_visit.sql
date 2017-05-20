6ff6593237ac7813b521aa1e0eedd6a61d8551db
Maxim S <atlamax@gmail.com>
2017-05-20 12:52:01 +0300

    CRUD for Meal/Activity/Review: Alter column visit.meal_id to "DEFAULT NULL"

---
+++ src/main/resources/db/migration/V1_2__alter_table_visit.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Alter Table `health_care_v1_dev`.`visit`
+-- -----------------------------------------------------
+ALTER TABLE `health_care_v1_dev`.`visit`
+    MODIFY `meal_id` INT UNSIGNED DEFAULT NULL;
