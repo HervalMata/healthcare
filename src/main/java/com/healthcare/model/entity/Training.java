364803a51c596b6fc31ec76aea76c5ddc8157c44
Jean Antunes <jantunes@ext.leroymerlin.com.br>
2017-05-24 00:58:16 -0300

    Training.java

--- src/main/java/com/healthcare/model/entity/Training.java
+++ src/main/java/com/healthcare/model/entity/Training.java
@@ -1,17 +1,11 @@
 package com.healthcare.model.entity;
 
-import java.io.Serializable;
-import java.sql.Timestamp;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "training")
@@ -19,20 +13,32 @@ import lombok.EqualsAndHashCode;
 public @Data class Training extends Audit implements Serializable {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -797138070537430162L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "title")
 	private String title;
 	@Column(name = "start_time")
 	private Timestamp startTime;
 	@Column(name = "end_time")
 	private Timestamp endTime;
+	@Column(name = "type")
 	private int type;
+	@Column(name = "trainer")
 	private String trainer;
+	@Column(name = "location")
 	private String location;
+	@Column(name = "note")
 	private String note;
+	@Column(name = "created_at")
+	private Timestamp createdAt;
+	@Column(name = "updated_at")
+	private Timestamp updatedAt;
 
+	public Long getId() {
+		return id;
+	}
 }
