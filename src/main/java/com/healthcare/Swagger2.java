0bc5d10b1d2009e09bcca90fccd4bb224a00c15e
qzhao6 <qzhao@operr.com>
2017-06-18 15:43:57 -0400

    Make swagger available only for dev

--- src/main/java/com/healthcare/Swagger2.java
+++ src/main/java/com/healthcare/Swagger2.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,8 +22,9 @@ public class Swagger2 {
 	}
 
 	private ApiInfo apiInfo() {
+		Contact contact = new Contact("danny", "http://www.healthcare.com", "dzhang@operr.com");
 		return new ApiInfoBuilder().title("Build RESTful APIs with Swagger2")
-				.description("more info, pelase http://www.operr.com").termsOfServiceUrl("http://www.operr.com")
-				.contact("healthcare").version("1.0").build();
+				.description("more info, pelase http://www.healthcare.com").termsOfServiceUrl("http://www.healthcare.com")
+				.contact(contact).version("1.0").build();
 	}
 }
