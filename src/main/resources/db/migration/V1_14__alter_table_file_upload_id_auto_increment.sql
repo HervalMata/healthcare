27e6e2979e1fdbe32d72fac5f535178cd0c2438e
Mostapha <Mostapha@Mostapha>
2017-06-20 02:48:35 +0200

    Document CRUD

---
+++ src/main/resources/db/migration/V1_14__alter_table_file_upload_id_auto_increment.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Alter Table `health_care_v1_dev`.`file_upload`
+-- -----------------------------------------------------
+ALTER TABLE `health_care_v1_dev`.`file_upload` 
+	MODIFY `id` INT UNSIGNED NOT NULL AUTO_INCREMENT;
