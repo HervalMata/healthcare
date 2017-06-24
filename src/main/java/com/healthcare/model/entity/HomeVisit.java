1abbc68587c5c374e38a20459d94e9e142dc3573
Fernando Paz <fpaz@engineer.com>
2017-06-23 19:25:51 -0500

    updated mapping for homevisit

--- src/main/java/com/healthcare/model/entity/HomeVisit.java
+++ src/main/java/com/healthcare/model/entity/HomeVisit.java
@@ -13,42 +13,40 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
-@Table(name = "visit")
-public @Data class Visit implements Serializable {
+@Table(name = "home_visit")
+@EqualsAndHashCode(callSuper = true)
+public @Data class HomeVisit extends Audit implements Serializable {
 
-	private static final long serialVersionUID = -5449963759010972006L;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	@ManyToOne
-	@JoinColumn(name = "agency_id")
-	private Agency agency;
 	@Column(name = "check_in_time")
 	private Timestamp checkInTime;
+	@Column(name = "check_out_time")
+	private Timestamp checkOutTime;
+	@ManyToOne
+	@JoinColumn(name = "meal_id")
+	private Meal meal;
+	@Column(name = "carereceiver_signature")
+	private String carereceiverSignature;
+	@Column(name = "carereceiver_comments")
+	private String carereceiverComments;
+	private String notes;
+	private String status;
 	@ManyToOne
 	@JoinColumn(name = "serviceplan_id")
 	private ServicePlan servicePlan;
 	@ManyToOne
-	@JoinColumn(name = "selected_meal_id")
-	private Meal selectedMeal;
-	@Column(name = "selected_table")
-	private String selectedTable;
-	@Column(name = "selected_seat")
-	private String selectedSeat;
-	@Column(name = "user_signature")
-	private String userSignature;
-	@Column(name = "user_barcode_id")
-	private String userBarcodeId;
-	@Column(name = "check_out_time")
-	private Timestamp checkOutTime;
-	@Column(name = "user_comments")
-	private String userComments;
-	private String notes;
-	private String status;
-	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-
+	@ManyToOne
+	@JoinColumn(name = "caregiver_id")
+	private Caregiver caregiver;
 }
