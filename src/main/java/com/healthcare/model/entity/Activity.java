dbdfc02061ec203b296c37f156b6814a512495ed
HiteshR90 <umaletiya.hitesh@gmail.com>
2017-07-07 16:39:26 +0530

    Implement Visit Activity  (#69)
    * Implement changes for login/logout admin and added test case
    
    * Implement Visit Activity Card
    
    * Renamed to correct version

--- src/main/java/com/healthcare/model/entity/Activity.java
+++ src/main/java/com/healthcare/model/entity/Activity.java
@@ -10,8 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "activity")
--- src/main/java/com/healthcare/repository/VisitActivityRepository.java
+++ src/main/java/com/healthcare/repository/VisitActivityRepository.java
@@ -1,9 +1,16 @@
 package com.healthcare.repository;
 
-import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.healthcare.model.entity.VisitActivity;
+import com.healthcare.model.entity.VisitActivityPK;
 
-public interface VisitActivityRepository extends Repository<VisitActivity, Long> {
-
+@Repository
+public interface VisitActivityRepository extends JpaRepository<VisitActivity, VisitActivityPK> {
+	 List<VisitActivity> findVisitActivityByActivityId(Long id);
+	 List<VisitActivity> findVisitActivityByVisitId(Long id);
 }
