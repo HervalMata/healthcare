4dd621cea1650c54168a8a6deebe986f1d8e25e7
Persada G <gilang.jtk07@gmail.com>
2017-07-09 06:51:05 +0800

    Updated role_id in User to be nullable

--- src/main/resources/db/migration/V1_25__alter_table_user_add_token_and_role.sql
+++ src/main/resources/db/migration/V1_25__alter_table_user_add_token_and_role.sql
@@ -3,7 +3,7 @@
 -- -----------------------------------------------------
 ALTER TABLE  ${db}.`user` 
 	ADD COLUMN `remember_token` VARCHAR(255) NULL DEFAULT NULL,
-	ADD COLUMN `role_id` INT UNSIGNED NOT NULL,
+	ADD COLUMN `role_id` INT UNSIGNED NULL,
 	ADD INDEX `fk_admin_role1_idx` (`role_id`),
   ADD CONSTRAINT `fk_user_role1`
     FOREIGN KEY (`role_id`)
