442213260c10735856b829a20f513315ec0ce2db
HiteshR90 <umaletiya.hitesh@gmail.com>
2017-07-12 21:32:59 +0530

    Change column from table to table_name in visit_has_activity table, because table is reserved word and cannot use as column

---
+++ src/main/resources/db/migration/V1_23__alter_table_visit_has_activity__rename_columnname_table.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Alter Table ${db}.`visit_has_activity`
+-- -----------------------------------------------------
+ALTER TABLE  ${db}.`visit_has_activity` 
+	CHANGE `table` `table_name` VARCHAR(255);		
